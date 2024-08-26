4e410580cbec64a0238a4add73157253f4419050
Kim Jihun <lumierebuds@gmail.com>
2024-08-26 11:48:26 +0900

    Update JenkinsProjectApplication.java

--- src/main/java/com/kh/socialLogin/JenkinsProjectApplication.java
+++ src/main/java/com/kh/socialLogin/JenkinsProjectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JenkinsProjectApplication {
-
+	// 변경사항이 생기면 빌드를 유발하는지 확인한다.
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsProjectApplication.class, args);
 	}
